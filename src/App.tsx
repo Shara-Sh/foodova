82fc27fec4cf008b376950321502c72c33889924
Shara <sharashahverdian@gmail.com>
2025-02-07 20:56:03 +0330

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex flex-col font-bold justify-center items-center h-screen bg-darkbg text-white">
+      <h1 className="text-5xl">Foodova</h1>
+      <h2 className="text-3xl">Comming soon...</h2>
+    </div>
+  );
+}
+
+export default App;
