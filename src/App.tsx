3a3ac2b963095d88b15a54b564dbb3790abada6e
Shara <sharashahverdian@gmail.com>
2025-02-07 23:52:13 +0330

    add filter button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import FilterCuisine from "./components/FilterCuisine";
 import FilterIngredient from "./components/FilterIngredient";
 import FilterMealType from "./components/FilterMealType";
@@ -5,6 +6,27 @@ import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 
 function App() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [isFilter, setIsFilter] = useState(window.innerWidth >= 1024);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+
+      // Always true for laptops, false for mobile
+      if (window.innerWidth >= 1024) {
+        setIsFilter(true);
+      } else {
+        setIsFilter(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="bg-darkbg h-screen text-white">
       <div className="mx-auto w-11/12 md:w-10/12">
@@ -13,11 +35,19 @@ function App() {
           <div className="flex-1">
             <SearchBar />
           </div>
-          <div className="space-y-4 md:w-1/4">
-            <FilterIngredient />
-            <FilterMealType />
-            <FilterCuisine />
-          </div>
+          <button
+            className="bg-lightgray text-lighttext hover:bg-lightgray-200 w-fit cursor-pointer rounded-xl px-4 py-2 hover:text-white md:hidden"
+            onClick={() => setIsFilter(!isFilter)}
+          >
+            Filter
+          </button>
+          {isFilter ? (
+            <div className="space-y-4 md:w-1/4">
+              <FilterIngredient />
+              <FilterMealType />
+              <FilterCuisine />
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
